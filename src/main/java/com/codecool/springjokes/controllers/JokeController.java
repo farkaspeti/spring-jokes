26d68ec18c7ae8d4a09e7a8d8749857edcbd0d59
farkaspeti <farkas.peti97@gmail.com>
2019-08-18 16:17:50 +0200

    JokeController implemented.

---
+++ src/main/java/com/codecool/springjokes/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package com.codecool.springjokes.controllers;
+
+import com.codecool.springjokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+    
+    @Autowired//Constructor DI the Autowired not required.
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    
+    
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        
+        model.addAttribute("joke", jokeService.getJoke());
+        
+        return "chucknorris";
+    }
+}
